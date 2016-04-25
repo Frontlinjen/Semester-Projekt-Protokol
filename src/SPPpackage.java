1598bb63150b6760763a96165192dd1b90dce2aa
Thomas Bech Madsen <thomasbm@live.dk>
2016-04-25 12:00:30 +0200

    Added SPPPackage Structure

--- src/SPPpackage.java
+++ src/SPPpackage.java
@@ -1,4 +1,54 @@
 
 public class SPPpackage {
+	private int seqnr;
+	private short checksum;
+	private short flags;
+	private byte[] data;
+	private int acknr;
 	
+	public void generateChecksum(){
+		
+	}
+	public void setAck(boolean value){
+		
+	}
+	public boolean isAck(){
+		
+	}
+	public void setSyn(boolean value){
+		
+	}
+	public boolean isSyn(){
+		
+	}
+	public void setRst(boolean value){
+		
+	}
+	public boolean isRst(){
+		
+	}
+	public void setFin(boolean value){
+		
+	}
+	public boolean isFin(){
+		
+	}
+	public int getAcknr() {
+		return acknr;
+	}
+	public void setAcknr(int acknr) {
+		this.acknr = acknr;
+	}
+	public int getSeqnr() {
+		return seqnr;
+	}
+	public void setSeqnr(int seqnr) {
+		this.seqnr = seqnr;
+	}
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
 }
