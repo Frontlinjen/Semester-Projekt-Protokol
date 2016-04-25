bbf14cc178b1458ed012d53a41653f9ee582bc7e
Thomas Bech Madsen <thomasbm@live.dk>
2016-04-25 15:56:59 +0200

    Started SPPserver

--- src/SPPserver.java
+++ src/SPPserver.java
@@ -1,4 +1,29 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SPPserver {
-
+	
+	DatagramSocket socket = null;
+	List<SPPpackage> buffer = new LinkedList<SPPpackage>();
+	int seqack = 0;
+	int seqsent;
+	
+	public void OpenSocket(int port){
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			System.out.println("Unable to open socket on port: " + port);
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void CreatePacket(byte[] data){
+		SPPpackage packet = new SPPpackage();
+		packet.setData(data);
+		packet.set   
+	}
+	
 }
