d3a7846091a056ef07f54cfa31b7eb511359b9d5
Hjorthen <Hjorthensebastian@gmail.com>
2016-04-30 13:50:06 +0200

    Implemented linkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,17 +1,44 @@
 
-
+//[]<->[]<->[]<->[]<->[]
+//					   ^
+//					   |
+//					 head
 public class LinkedList<T> {
 	
-	public Node<T> head;
+	private Node<T> head;
 	
 	//Indsætter en node i listen.
-	public void insert(Node<T> prev){
+	public void insert(Node<T> prev, T object){
+		Node<T> newNode = new Node<T>(object, prev, prev.next);
+		if(prev==head)
+		{
+			head = newNode;
+		}
+		if(prev.next!=null)
+		{
+			prev.next.prev = newNode;
+		}
+		prev.next = newNode;
 		
 	}
 	
+	Node<T> get(int index)
+	{
+		Node<T> n = head;
+		for(int i=0;i<index;++i)
+		{
+			if(n==null)
+				return null;
+			n = n.prev;
+		}
+		return n;
+	}
 	//Tilføjer et objekt efter head.
 	public void insert(T object){
-		
+		Node<T> newNode = new Node<T>(object, head, null);
+		if(head!=null)
+			head.next = newNode;
+		head = newNode;
 	}
 	
 	public Node<T> getHead(){
@@ -19,7 +46,26 @@ public class LinkedList<T> {
 	}
 	
 	public void remove(Node<T> n){
-		
+		if(n.prev!=null)
+			n.prev.next = n.next;
+		if(n.next!=null)
+			n.next.prev = n.prev; 
 	}
 	
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.insert(1);
+		list.insert(2);
+		list.insert(3);
+		list.insert(4);
+		list.insert(list.get(1), 5);
+		
+		
+		Node<Integer> node = list.head;
+		while(node!=null)
+		{
+			System.out.println(node.key);
+			node = node.prev;
+		}
+	}
 }
