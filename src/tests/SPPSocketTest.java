a6fe2227d3f371b3dfb6492bc8bfbe117ed8608a
SenadBegovic <senadbegovic2500@hotmail.com>
2016-05-02 16:28:23 +0200

    Created SPPSocketTest

---
+++ src/tests/SPPSocketTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import SPP.SPPpacket;
+
+public class SPPSocketTest {
+
+
+	public static void main(String[]args) throws IOException{
+		(new Server2()).start();
+		DatagramSocket socket = new DatagramSocket(32000);
+		SPPpacket spp = new SPPpacket();
+		spp.setData("Test".getBytes());
+		byte[] data = spp.getByteStream();
+		DatagramPacket packet = new DatagramPacket(data, data.length);
+		packet.setPort(33000);
+		packet.setAddress(InetAddress.getByName("localhost"));
+		socket.send(packet);
+		
+	}
+	
+
+}
+
+class Server2 extends Thread{
+
+	public void run() {
+			try {
+				DatagramSocket server = new DatagramSocket(33000);
+				byte[] inData = new byte[128];
+				DatagramPacket packet = new DatagramPacket(inData, inData.length);
+				server.receive(packet);
+				//Copies only the data recived and removed the nulls 
+				byte[] data = new byte[packet.getLength()];
+				for (int i = 0; i < data.length; i++) {
+					data[i] = inData[i];
+				}
+		
+				SPPpacket p = new SPPpacket(data);
+				for(byte b : p.getData())
+				{
+					System.out.print(b + " ");
+				}
+				System.out.print("\n");
+				String s = new String(p.getData());
+				System.out.println(s);
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	
+}
