e1317979c1d9a6ad372ec94526531cd8a2cb159f
Nicki <Nicki@10.16.107.26>
2016-05-02 17:19:50 +0200

    Added outprints for debugging/clearence

--- src/SPP/SPPSocket.java
+++ src/SPP/SPPSocket.java
@@ -12,6 +12,7 @@ public class SPPSocket {
 	private SPPpacket packet;
 	private DatagramSocket socket;
 	final int MAX_PACKETSIZE = 2048;
+	
 	public SPPSocket(DatagramSocket s, int remotePort, InetAddress address, int clientSeq)
 	{
 		socket = s;
@@ -23,15 +24,18 @@ public class SPPSocket {
 	{
 		this(localPort < 0 ? new DatagramSocket() : new DatagramSocket(localPort), remotePort, address, startSeq);
 	}
+	
 	public void sendData(byte[] data)
 	{
 		SPPpacket newPacket = new SPPpacket();
 		newPacket.setData(data);
 		sendPacket(newPacket);
+		System.out.println("The data has been sent.");
 	}
 	public void sendPacket(SPPpacket p)
 	{
 		server.Send(p);
+		System.out.println("the following packet has been sent: " + p.getSeqnr());
 	}
 	public void setClientSeqNr(int seqnr){
 		client.setSeqNr(seqnr);
@@ -39,32 +43,35 @@ public class SPPSocket {
 	public SPPpacket getPacket()
 	{
 		do{
-		try
-		{
-			byte[] inBuffer = new byte[MAX_PACKETSIZE];
-			DatagramPacket recievePacket = new DatagramPacket(inBuffer, inBuffer.length);
-			socket.receive(recievePacket);
-			
-			//Copies only the data received and removed the nulls 
-			byte[] data = new byte[recievePacket.getLength()];
-			for (int i = 0; i < data.length; i++) {
-				data[i] = inBuffer[i];
-			}
-			SPPpacket newPacket = new SPPpacket(data);
-			
-			if(packet.getChecksum() == packet.calculateChecksum()){
-			
-				client.recievePacket(newPacket);
-				
-				SPPpacket newerpacket = new SPPpacket(data);
-				newerpacket.setAck();
-				newerpacket.setAcknr(newPacket.getSeqnr());
-				server.Send(newerpacket);
-				
-			if(newPacket.isAck())
+			try
 			{
-				server.OnAckRecieved(newPacket.getAcknr());
-			}
+				byte[] inBuffer = new byte[MAX_PACKETSIZE];
+				DatagramPacket recievePacket = new DatagramPacket(inBuffer, inBuffer.length);
+				socket.receive(recievePacket);
+				
+				//Copies only the data received and removed the nulls 
+				byte[] data = new byte[recievePacket.getLength()];
+				for (int i = 0; i < data.length; i++) {
+					data[i] = inBuffer[i];
+				}
+				SPPpacket newPacket = new SPPpacket(data);
+				
+				if(packet.getChecksum() == packet.calculateChecksum()){
+					System.out.println("The checksum matches.");
+					
+					client.recievePacket(newPacket);
+					
+					SPPpacket newerpacket = new SPPpacket(data);
+					newerpacket.setAck();
+					newerpacket.setAcknr(newPacket.getSeqnr());
+					server.Send(newerpacket);
+					System.out.println("The following acknr has been sent: " + newerpacket);
+					
+				if(newPacket.isAck())
+				{
+					server.OnAckRecieved(newPacket.getAcknr());
+					System.out.println("newPacket.isAck = " + newPacket.isAck() + " OnAckRecieved is executed with ackNr = " + newPacket.getAcknr());
+				}
 			}
 		}
 		catch(IOException e)
