0a0ed133b9309704aea5b3791fb97bf0e16db02a
Hjorthen <Sebastian@10.16.102.87>
2016-05-02 12:26:44 +0200

    Added sending logic

--- src/SPP/ServerSocket.java
+++ src/SPP/ServerSocket.java
@@ -1,15 +1,61 @@
 package SPP;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class ServerSocket {
 
+	DatagramSocket socket = null;
 	
 	public ServerSocket(int port)
 	{
-		
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public SPPSocket accept()
 	{
-		
+		SPPpacket newPacket = null;
+		DatagramPacket recievePacket = null;
+		System.out.println("Waiting for connection...");
+		do
+		{
+			byte[] inBuffer = new byte[2048];
+			recievePacket = new DatagramPacket(inBuffer, inBuffer.length);
+			socket.receive(recievePacket);
+			
+			//Copies only the data recived and removed the nulls 
+			byte[] data = new byte[recievePacket.getLength()];
+			for (int i = 0; i < data.length; i++) {
+				data[i] = inBuffer[i];
+			}
+			newPacket = new SPPpacket(data);
+			//If not a syn packet, ask them to try and reconnect!
+			if(!newPacket.isSyn())
+			{
+				System.out.println("Recieved non-syn packet");
+				SPPpacket rstPacket = new SPPpacket();
+				rstPacket.setRst();
+				byte[] byteStream = rstPacket.getByteStream();
+				//The RST packet can be fire and forget, hence we dont need 
+				try {
+					socket.send(new DatagramPacket(byteStream, byteStream.length, recievePacket.getAddress(), recievePacket.getPort()));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}while(!newPacket.isSyn());
+		do
+		{
+			SPPSocket connection = new SPPSocket(socket, recievePacket.getPort(), recievePacket.getAddress(), (int)(Math.random()%Integer.MAX_VALUE));
+		}while(true);
+		System.out.println();
 	}
 	
 }
